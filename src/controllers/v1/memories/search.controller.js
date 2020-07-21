d7ea96630d8cfae7dbab2fd65ca7dd35af62168c
Gayan Kumarage <gayan_ac@hotmail.com>
2020-07-21 12:28:59 +0530

    search api implemented to return results by searching through sample data

--- src/controllers/v1/memories/search.controller.js
+++ src/controllers/v1/memories/search.controller.js
@@ -1,11 +1,22 @@
-const sampleData = require("./sampleData.json");
+const { previews } = require("./sampleData.json");
 
 const SearchMemory = (req, res) => {
+ const searchResutls = searchResults(req.query.query) || [];
+
  return res
- .status(200)
- .set("Content-Type", "application/json")
-  .send(JSON.stringify(sampleData.previews[0]));
+  .status(200)
+  .set("Content-Type", "application/json")
+  .send(JSON.stringify(searchResutls));
 };
+
+function searchResults(query) {
+ return previews.filter((preview) => {
+  const serachStr = `${preview.title} ${preview.year} ${preview.location.reigon} ${preview.location.country}`;
+  const regex = new RegExp(query, "i");
+
+  return regex.test(serachStr);
+ });
+}
 
 module.exports = {
  SearchMemory,
